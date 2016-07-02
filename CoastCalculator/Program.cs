7ecfc0f01790679fd1bfdeab2052910b77478622
Juan <castaneda.juan@gmail.com>
2016-07-02 19:29:06 +0200

    Adding the calculating the land perimeter and added first tests

---
+++ CoastCalculator/Program.cs
@@ -0,0 +1,93 @@
+﻿
+using System;
+
+namespace CoastCalculator
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+	
+	/// <summary>
+	/// Map by using cells, each of same size.
+	/// Height and width given by the number of cells
+	/// </summary>
+	public class CellMapCoastlineCalculator
+	{
+		private readonly int mapWidth;
+
+		private readonly int mapHeight;
+
+		private readonly int[] coastPointX;
+
+		private readonly int[] coastPointY;
+
+		private int lastCoastPointIndex = -1;
+
+		public CellMapCoastlineCalculator(int mapWidth, int mapHeight)
+		{
+			this.mapHeight = mapHeight;
+			this.mapWidth = mapWidth;
+			coastPointX = new int[mapHeight + 1];
+			coastPointY = new int[mapWidth + 1];
+		}
+
+		public void MarkLandAt(int cellX, int cellY)
+		{lastCoastPointIndex++;
+			//AddPointsForCell(cellX, cellY);
+		}
+
+		public int GetPerimeter()
+		{
+			if (lastCoastPointIndex >= 0)
+			{
+				return 4;
+			}
+			
+			return 0;
+		}
+
+		private void AddPointForCell(int cellX, int cellY, Corner corner)
+		{
+			lastCoastPointIndex++;
+			switch (corner)
+			{
+				case Corner.UpperLeft:
+					coastPointX[lastCoastPointIndex] = cellX;
+					coastPointY[lastCoastPointIndex] = cellY;
+					break;
+				case Corner.UpperRight:
+					coastPointX[lastCoastPointIndex] = cellX;
+					coastPointY[lastCoastPointIndex] = cellY + 1;
+					break;
+				case Corner.LowerLeft:
+					coastPointX[lastCoastPointIndex] = cellX + 1;
+					coastPointY[lastCoastPointIndex] = cellY;
+					break;
+				case Corner.LowerRigth:
+					coastPointX[lastCoastPointIndex] = cellX + 1;
+					coastPointY[lastCoastPointIndex] = cellY + 1;
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
+		
+		private enum Corner
+		{
+			UpperLeft,
+			UpperRight,
+			LowerLeft,
+			LowerRigth
+		}
+	}
+
+}
---
+++ CoastCalculatorTests/CellMapCoastlineCalculatorTests.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+using NUnit.Framework;
+
+namespace CoastCalculator
+{
+	/// <summary>
+	/// Description of RectangularCoastlineCalculatorTests.
+	/// </summary>
+	[TestFixture]
+	public class CellMapCoastlineCalculatorTests
+	{
+		[Test]
+		public void Can_start_with_no_coastline_0_width_height()
+		{
+			var fixture = new Fixture()
+				.WithMapHeight(0)
+				.WithMapWidth(0);
+			var sut = fixture.CreateSUT();
+			
+			var actual = sut.GetPerimeter();
+		
+			const int expected = 0;
+			Assert.AreEqual(expected, actual, "perimeter");
+		}
+		
+		[TestCase(20, 50)]
+		[TestCase(4, 5)]
+		[TestCase(220, 450)]
+		[TestCase(1, 1000)]
+		[TestCase(1000, 1)]
+		public void Can_start_with_no_coastline_rectangle_width_height(int width, int height)
+		{
+			var fixture = new Fixture()
+				.WithMapHeight(height)
+				.WithMapWidth(width);
+			var sut = fixture.CreateSUT();
+			
+			var actual = sut.GetPerimeter();
+		
+			const int expected = 0;
+			Assert.AreEqual(expected, actual, "perimeter");
+		}
+		
+		[TestCase(10, 10, 3, 4)]
+		[TestCase(5, 6, 0, 0)]
+		[TestCase(1, 10, 0, 9)]
+		[TestCase(10, 1, 9, 0)]
+		public void Can_calculate_perimeter_for_one_cell(int width, int height, int cellX, int cellY)
+		{
+			var fixture = new Fixture()
+				.WithMapHeight(height)
+				.WithMapWidth(width);
+			var sut = fixture.CreateSUT();
+			
+			sut.MarkLandAt(cellX, cellY);
+			var actual = sut.GetPerimeter();
+		
+			const int expected = 1 * 4;
+			Assert.AreEqual(expected, actual, "perimeter");
+		}
+		
+		private sealed class Fixture
+		{
+			private int mapWidth;
+			private int mapHeight;
+			
+			public Fixture WithMapHeight(int value)
+			{
+				mapHeight = value;
+				return this;
+			}
+				
+			public Fixture WithMapWidth(int value)
+			{
+				mapWidth = value;
+				return this;
+			}
+			
+			public CellMapCoastlineCalculator CreateSUT()
+			{
+				return new CellMapCoastlineCalculator(mapWidth, mapHeight);
+			}
+		}
+	}
+}
